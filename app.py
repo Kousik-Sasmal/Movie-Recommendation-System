69fedc7ae6fec0645ded44c715ce367090658a9b
Kousik Sasmal <kousik712417.com>
2023-05-26 16:35:54 +0530

    modified app.py

--- app.py
+++ app.py
@@ -2,19 +2,13 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import pickle
-import requests
-import database
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
 
+from utils import similarity
+from utils import poster_fetch
 
 popular_df = pickle.load(open('artifacts/popular_df.pkl','rb'))
 genres_group = pickle.load(open('artifacts/genres_group.pkl','rb'))
 new_df = pd.read_csv('artifacts/new_df.csv')
-
-cv = CountVectorizer(max_features=5000,stop_words='english')
-vectors = cv.fit_transform(new_df['tags']).toarray()
-similarity = cosine_similarity(vectors)
 
 
 def recommend(movie):
@@ -28,30 +22,6 @@ def recommend(movie):
         
     return movies,movie_id
 
-
-def poster_fetch(movie_ids):
-    unload_image_url = 'https://static.vecteezy.com/system/resources/previews/004/141/669/non_2x/no-photo-or-blank-image-icon-loading-images-or-missing-image-mark-image-not-available-or-image-coming-soon-sign-simple-nature-silhouette-in-frame-isolated-illustration-vector.jpg'
-   
-    full_paths = []
-    for _id in movie_ids:
-        if database.search(_id)!=0:
-            full_paths.append(database.search(_id))
-        else:
-            try:
-                url = "https://api.themoviedb.org/3/movie/{}?api_key=879d1be008918a158ad109a08fd6d820".format(_id)
-                response = requests.get(url)
-                data = response.json()
-                
-                path = data['poster_path']
-                full_path = 'https://image.tmdb.org/t/p/w780' + path
-                full_paths.append(full_path)
-
-                database.insert(_id,full_path)
-
-            except:
-                full_paths.append(unload_image_url)         
-
-    return full_paths
 
 def get_movie_details(genre):
     movie_ids = []
@@ -83,6 +53,21 @@ def showing_movie_details(num_movies,movie_titles,movie_posters):
                 break
 
 
+def load_recommended_movies():
+    st.header('Recommended Movies')
+
+    selected_movie = st.selectbox('Select a movie',new_df['title'].tolist())
+    btn1 = st.button('Recommend')
+    if btn1:
+        movies,ids = recommend(selected_movie)
+        poster_image_urls = poster_fetch(ids)
+
+        cols = st.columns(6)
+        for i in range(6):
+            cols[i].image(poster_image_urls[i])
+            cols[i].write(f"**{movies[i]}**")  #title
+
+
 def load_genre_wise_popular_movies():
     st.header('Genre-wise Popular Movies')
     unique_genres = list(set(popular_df['genres'].explode()))
@@ -111,20 +96,6 @@ def load_overall_popular_movies():
 
         showing_movie_details(num_movies,movie_titles,movie_posters)
 
-
-def load_recommended_movies():
-    st.header('Recommended Movies')
-
-    selected_movie = st.selectbox('Select a movie',new_df['title'].tolist())
-    btn1 = st.button('Recommend')
-    if btn1:
-        movies,ids = recommend(selected_movie)
-        poster_image_urls = poster_fetch(ids)
-
-        cols = st.columns(6)
-        for i in range(6):
-            cols[i].image(poster_image_urls[i])
-            cols[i].write(f"**{movies[i]}**")  #title
 
 
 
