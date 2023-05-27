9f4a15cd3fb4a8565261aa459e4b6c80cba44141
Kousik Sasmal <kousik712417.com>
2023-05-27 12:53:48 +0530

    added comment

--- database.py
+++ database.py
@@ -1,10 +1,16 @@
 import json
 import streamlit as st
 
-
+# if any problem in image_url searching, we will return this image_url (url of an unload image)
 unload_image_url = 'https://static.vecteezy.com/system/resources/previews/004/141/669/non_2x/no-photo-or-blank-image-icon-loading-images-or-missing-image-mark-image-not-available-or-image-coming-soon-sign-simple-nature-silhouette-in-frame-isolated-illustration-vector.jpg'
 
-def search(movie_id):       
+
+def search(movie_id):
+    """
+    Takes an id of a movie. 
+    Searches the in the database `poster_image_url.json`.
+    Returns an `image url` of that `movie_id` if available.
+    """
     try:
         with open('poster_image_url.json','r') as rf:
             images_data = json.load(rf)
@@ -18,10 +24,19 @@ def search(movie_id):
         st.write("DO NOT CHANGE input until get results")
         return unload_image_url
 
-def insert(movie_id,image_url):
-    with open('poster_image_url.json','r') as rf:
-        images_data = json.load(rf)
-        images_data[str(movie_id)] = image_url
 
-    with open('poster_image_url.json','w') as wf:
-        json.dump(images_data,wf,indent=4)
+def insert(movie_id,image_url):
+    """
+    Takes two input, an id of a movie and image url. 
+    Inserts an `image url` of that `movie_id` into the database `poster_image_url.json`.
+    """
+    try:
+        with open('poster_image_url.json','r') as rf:
+            images_data = json.load(rf)
+            images_data[str(movie_id)] = image_url
+
+        with open('poster_image_url.json','w') as wf:
+            json.dump(images_data,wf,indent=4)
+
+    except:
+        pass
